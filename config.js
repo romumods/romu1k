46ce541cd90a1d7b6234db9a9e09bba78761b8bf
romumods <60108499+romumods@users.noreply.github.com>
2024-01-15 20:40:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,17 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['33757054415', 'Abraham Dwamena3', true],
+  ['265886949444', 'Splash', true], 
+  ['265885321892', 'Robert', true],
+  ['265994420911', 'Splash', true], 
   
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "265886949444" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','33757054415','233208179431'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087','33757054415']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','33757054415']
+global.mods = ['265886949444', '265885321892', '265994420911'] 
+global.prems = ['265886949444', '265885321892', '265994420911']
+global.allowed = ['265886949444', '265885321892', '265994420911']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -55,16 +52,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'Splash-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'SPLASH TEAM' 
 global.author = '@XLICON-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
+global.igfg = '▢✓ Follow Me on Tiktok\nhttps://tiktok.com/@romu1k\n' 
+global.dygp = 'https://instagram.com/romu1k'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
+global.fgyt = 'https://youtube.com/@romu1k'
+global.fgpyp = 'https://youtube.com/@romu1k'
 global.fglog = 'XLICON.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
